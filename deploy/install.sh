2ac7c7716c55678323f11f759554ffa0f2f49f86
暘 袁 <frozenpine@live.cn>
2017-09-01 18:18:45 +0800

    修正rpm安装逻辑

--- deploy/install.sh
+++ deploy/install.sh
@@ -186,7 +186,9 @@ _makeVirtualEnv() {
 _rpmInstall() {
     if [[ -d "${BASE_DIR}/packages" ]]; then
         pushd "${BASE_DIR}/packages" &>/dev/null
-        rpm -ivh *.${RELEASE}*.rpm
+        for pkg in `ls *.${RELEASE}*.rpm`; do
+            rpm -ivh ${pkg}
+        done
         _info "Pre-install packages finished."
         popd &>/dev/null
     else
