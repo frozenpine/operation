28d0a0d06ecd9721db088cf2b6a937e188fa3fea
Dell-Pc <guanyf@quantdo.com.cn>
2017-10-25 16:11:05 +0800

    20171025 新增skip next
    Signed-off-by: Dell-Pc <guanyf@quantdo.com.cn>

--- TaskManager/controller_msg.py
+++ TaskManager/controller_msg.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from enum import Enum
+
 msg_dict = {-1: u"队列初始化失败",
             -11: u"队列为空",
             -12: u"队列不存在",
@@ -9,3 +11,15 @@ msg_dict = {-1: u"队列初始化失败",
             12: u"任务执行中",
             13: u"任务等待中",
             14: u"任务失败"}
+
+
+class MsgInfo(Enum):
+    InitFailed = -1
+    QueueEmpty = -11
+    QueueNotExits = -12
+    QueueFailNotRecoverable = -13
+    OK = 0
+    JobIssued = 11
+    JobRunning = 12
+    JobWaiting = 13
+    JobFailed = 14
