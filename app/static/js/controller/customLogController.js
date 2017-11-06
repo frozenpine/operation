9d283bae28a9ef5551b0ba699ad6547ac6524776
暘 袁 <frozenpine@live.cn>
2017-11-06 15:57:22 +0800

    新增查询自定义日志、数据表的过渡界面

--- app/static/js/controller/customLogController.js
+++ app/static/js/controller/customLogController.js
@@ -1,13 +1,15 @@
 app.controller('customLogController', ['$scope', '$http', function($scope, $http){
-    $scope.servers = [];
+    $scope.checking = true;
     $http.get('api/system/id/3/customlogs')
         .success(function(response){
             console.log(response);
             if (response.error_code === 0) {
                 $scope.servers = response.data.records;
+                $scope.checking = false;
             }
         })
         .error(function(response){
-
+            $scope.checking = false;
+            $scope.servers = [];
         });
 }]);
--- app/static/js/controller/customSqlController.js
+++ app/static/js/controller/customSqlController.js
@@ -1,13 +1,19 @@
 app.controller('customSqlController', ['$scope', '$http', function($scope, $http){
-    $scope.dbs = [];
+    $scope.checking = true;
     $http.get('api/system/id/3/customsqls')
         .success(function(response){
             console.log(response);
             if (response.error_code === 0) {
                 $scope.dbs = response.data.records;
+                $scope.checking = false;
             }
         })
-        .error(function(response){
-
+        .error(function(response) {
+            $scope.checking = false;
+            $scope.dbs = [];
         });
+
+    $scope.getCustomSqlDs = function () {
+
+    };
 }]);
