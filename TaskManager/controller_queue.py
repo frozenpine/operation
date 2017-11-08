86da4d912f621dacd5b98360c6c38a390e8dd752
Dell-Pc <guanyf@quantdo.com.cn>
2017-11-08 13:38:04 +0800

    20171108 更新skip模块
    Signed-off-by: Dell-Pc <guanyf@quantdo.com.cn>

--- TaskManager/controller_queue.py
+++ TaskManager/controller_queue.py
@@ -110,7 +110,7 @@ class ControllerQueue(object):
         for each in self.controller_task_result_list:
             if each.get('task_uuid') == task_uuid:
                 task_status = (4, u"任务跳过成功")
-                each.update({'task_status': task_status})
+                each.get('task_uuid').update({'task_status': task_status})
         if not fail_task_uuid_list:
             return -1, u"队列无失败任务"
         self.controller_queue_status = 0
