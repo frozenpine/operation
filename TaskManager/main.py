ba9fe685f0ac773c554f33ba7b0f87e5898b6f56
关轶凡 <guanyf@quantdo.com.cn>
2017-12-13 16:52:04 +0800

    20171213 更新socket通信异常处理
    Signed-off-by: 关轶凡 <guanyf@quantdo.com.cn>

--- TaskManager/main.py
+++ TaskManager/main.py
@@ -33,8 +33,7 @@ if __name__ == "__main__":
     worker.register_callback("init_callback", controller.worker_init_callback)
     worker.register_callback("start_callback", controller.worker_start_callback)
     worker.register_callback("end_callback", controller.worker_end_callback)
-    SocketServer(
-        controller.worker_init_callback, controller.worker_start_callback, controller.worker_end_callback).start()
-
+    SocketServer(controller.worker_init_callback, controller.worker_start_callback, controller.worker_end_callback,
+                 socket_port).start()
     controller.register_callback("kill_callback", worker.kill_process_callback)
     gevent.joinall([gevent.spawn(server.run), gevent.spawn(worker.loop)])
--- TaskManager/socket_conn.py
+++ TaskManager/socket_conn.py
@@ -9,11 +9,12 @@ from TaskManager import tm_logger as logging
 
 
 class SocketServer(Thread):
-    def __init__(self, init_callback, start_callback, end_callback):
+    def __init__(self, init_callback, start_callback, end_callback, socket_port):
         Thread.__init__(self)
+        socket_port = int(socket_port)
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server.bind(('127.0.0.1', 7000))
+        self.server.bind(('127.0.0.1', int(socket_port)))
         self.server.listen(10)
         self.init_callback = init_callback
         self.start_callback = start_callback
@@ -47,7 +48,7 @@ class SocketServer(Thread):
                 if info.type() == "end":
                     self.end_callback(info)
             else:
-                logging.warning('unexpected socket received: {0}'.format(info.to_str()))
+                logging.warning('[server] unexpected socket received: {0}'.format(info.to_str()))
 
 
 class SocketClient(object):
@@ -55,11 +56,13 @@ class SocketClient(object):
     def send(cls, data):
         socket_port = environ.get("SOCKET_PORT") or 7000
         socket_timeout = environ.get("SOCKET_TIMEOUT") or 5
+        socket_port, socket_timeout = int(socket_port), int(socket_timeout)
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect(('127.0.0.1', socket_port))
         dump_data = pickle.dumps(data)
         # 计算dump之后消息长度
         logging.info('[client] info length: {0}'.format(len(dump_data)))
+        # debug模式下
         logging.debug('[client] info: {0}'.format(dump_data))
         retry_count = 0
         while 1:
@@ -75,7 +78,7 @@ class SocketClient(object):
             except socket.timeout:
                 # 未收到server端确认信息
                 retry_count += 1
-                logging.warn('[client] retry: {0}'.format(retry_count))
+                logging.warning('[client] retry: {0}'.format(retry_count))
             else:
                 break
         # 收到server端确认消息
