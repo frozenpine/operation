7b39591405fff5546c9f2c83df97932dcbf5aa46
袁暘 <yuanyang@quantdo.com.cn>
2018-01-18 16:18:42 +0800

    201801181618

--- init.py
+++ init.py
@@ -1,8 +1,6 @@
 # -*- coding: UTF-8 -*-
-import os
 
 import yaml
-from flask import url_for
 from flask.testing import EnvironBuilder
 from flask_script import Manager
 
@@ -14,21 +12,15 @@ manager = Manager(test_app)
 
 
 def _encrypt(match):
-    return match.group(1) + \
-           AESCrypto.encrypt(
-               match.group(2),
-               test_app.config['SECRET_KEY']
-           ) + \
-           match.group(3)
+    return (match.group(1),
+            AESCrypto.encrypt(match.group(2), test_app.config['SECRET_KEY']),
+            match.group(3))
 
 
 def _decrypt(match):
-    return match.group(1) + \
-           AESCrypto.decrypt(
-               match.group(2),
-               test_app.config['SECRET_KEY']
-           ) + \
-           match.group(3)
+    return (match.group(1),
+            AESCrypto.decrypt(match.group(2), test_app.config['SECRET_KEY']),
+            match.group(3))
 
 
 @manager.command
@@ -55,10 +47,10 @@ def init_auth():
     users = {}
     user_role_relation = {}
     for user in auth['Users']:
-        if user.has_key('roles'):
+        if 'roles' in user:
             for role in user['roles']:
-                if roles.has_key(role):
-                    if not user_role_relation.has_key(role):
+                if role in roles:
+                    if role not in user_role_relation:
                         user_role_relation[role] = []
                     user_role_relation[role].append(user['login'])
             user.pop('roles')
@@ -77,9 +69,9 @@ def init_auth():
     for pri in auth['Privileges']:
         typ, cat = pri['bit'].split('.')
         pri['bit'] = getattr(globals()[typ], cat).value
-        if pri.has_key('roles'):
+        if 'roles' in pri:
             for role in pri.pop('roles'):
-                if not role_privilege_relation.has_key(role):
+                if role not in role_privilege_relation:
                     role_privilege_relation[role] = []
                 role_privilege_relation[role].append(
                     "{uri}#{bit}".format(uri=pri['uri'], bit=pri['bit'])
@@ -102,7 +94,7 @@ def init_inventory():
 
     servers = {}
     for svr in inventory['Servers']:
-        if svr.has_key('platform'):
+        if 'platform' in svr:
             typ, cat = svr['platform'].split('.')
             svr['platform'] = getattr(globals()[typ], cat).value
         servers[svr['uuid']] = Server(**svr)
@@ -117,8 +109,8 @@ def init_inventory():
 
     systems = {}
     for sys in inventory['Systems']:
-        if sys.has_key('type'):
-            if sys_types.has_key(sys['type']):
+        if 'type' in sys:
+            if sys['type'] in sys_types:
                 typ = sys_types[sys.pop('type')]
             else:
                 typ = SystemType.find(name=sys.pop('type'))
@@ -130,7 +122,7 @@ def init_inventory():
 
     processes = {}
     for proc in inventory['Processes']:
-        if proc.has_key('type'):
+        if 'type' in proc:
             typ, cat = proc['type'].split('.')
             proc['type'] = getattr(globals()[typ], cat).value
         proc['sys_id'] = systems[proc.pop('system')].id
@@ -141,10 +133,10 @@ def init_inventory():
 
     sockets = {}
     for sock in inventory['Sockets']:
-        if sock.has_key('direction'):
+        if 'direction' in sock:
             typ, cat = sock['direction'].split('.')
             sock['direction'] = getattr(globals()[typ], cat).value
-        if sock.has_key('process'):
+        if 'process' in sock:
             sock['proc_id'] = processes[sock.pop('process')].id
         sockets[sock['uuid']] = Socket(**sock)
     db.session.add_all(sockets.values())
@@ -155,23 +147,6 @@ def init_inventory():
             systems[parent].child_systems.append(systems[child])
     db.session.add_all(systems.values())
     db.session.commit()
-
-    # datasources = []
-    # for ds in inventory['DataSources']:
-    #     ds['sys_id'] = systems[ds.pop('system')].id
-    #     typ, cat = ds['src_type'].split('.')
-    #     ds['src_type'] = getattr(globals()[typ], cat).value
-    #     typ, cat = ds['src_model'].split('.')
-    #     ds['src_model'] = getattr(globals()[typ], cat).value
-    #     if test_app.config['GLOBAL_ENCRYPT']:
-    #         ds['source']['uri'] = re.sub(
-    #             '^(.+://[^:]+:)([^@]+)(@.+)$',
-    #             _encrypt,
-    #             ds['source']['uri']
-    #         )
-    #     datasources.append(DataSource(**ds))
-    # db.session.add_all(datasources)
-    # db.session.commit()
 
     config_files = []
     for conf in inventory['ConfigFiles']:
@@ -184,30 +159,18 @@ def init_inventory():
 
 
 @manager.command
-def init_sockets():
-    f = open('inventory.yml')
-    inventory = yaml.load(f)
-
-    sockets = {}
-    for sock in inventory['Sockets']:
-        if sock.has_key('process'):
-            proc = TradeProcess.find(name=sock.pop('process'))
-            if proc:
-                sock['proc_id'] = proc.id
-        sockets[sock['name'] + sock['uri']] = Socket(**sock)
-    db.session.add_all(sockets.values())
-    db.session.commit()
-
-
-@manager.command
 def init_operation():
     f = open('operations.yml')
     opers = yaml.load(f)
 
     catalogs = {}
     for cata in opers['OperationCatalog']:
-        catalog = OperationCatalog(**cata)
-        catalogs[catalog.name] = catalog
+        check = OperationCatalog.find(name=cata['name'])
+        if check:
+            catalogs[check.name] = check
+        else:
+            catalog = OperationCatalog(**cata)
+            catalogs[catalog.name] = catalog
     db.session.add_all(catalogs.values())
     db.session.commit()
 
@@ -221,160 +184,30 @@ def init_operation():
                     bk['detail']['remote']['params']['password'],
                     test_app.config['SECRET_KEY']
                 )
-        if bk.has_key('sys_id') and bk.has_key('system'):
+        if 'sys_id' in bk and 'system' in bk:
             bk.pop('system')
-        elif bk.has_key('system'):
+        elif 'system' in bk:
             name = bk.pop('system')
             if re.match(r'[\da-zA-Z]{8}-(?:[\da-zA-Z]{4}-){3}[\da-zA-Z]{12}', name):
                 sys = TradeSystem.find(uuid=name)
             else:
                 sys = TradeSystem.find(name=name)
             bk['sys_id'] = sys.id
-        if bk.has_key('catalog'):
+        if 'catalog' in bk:
             bk['catalog_id'] = catalogs[bk.pop('catalog')].id
         operation_book[bk['name']] = OperationBook(**bk)
     db.session.add_all(operation_book.values())
     db.session.commit()
 
-    '''groups = {}
-    for grp in opers['OperationGroups']:
-        if grp.has_key('sys_id') and grp.has_key('system'):
-            grp.pop('system')
-        elif grp.has_key('system'):
-            name = grp.pop('system')
-            if re.match(r'[\da-zA-Z]{8}-(?:[\da-zA-Z]{4}-){3}[\da-zA-Z]{12}', name):
-                sys = TradeSystem.find(uuid=name)
-            else:
-                sys = TradeSystem.find(name=name)
-            grp['sys_id'] = sys.id
-        groups[grp['name']] = OperationGroup(**grp)
-    db.session.add_all(groups.values())
-    db.session.commit()
-
-    operations = []
-    for op in opers['Operations']:
-        if op.has_key('op_group_id') and op.has_key('group'):
-            op.pop('group')
-        elif op.has_key('group'):
-            op['op_group_id'] = groups[op.pop('group')].id
-        if op.has_key('book_id') and op.has_key('op_book'):
-            op.pop('op_book')
-        elif op.has_key('op_book'):
-            op['book_id'] = operation_book[op.pop('op_book')].id
-        operations.append(Operation(**op))
-    db.session.add_all(operations)
-    db.session.commit() '''
-
 
 @manager.command
-def global_encrypt():
+def encrypt():
     pass
 
 
 @manager.command
-def modify_system(option_file):
-    if os.path.exists(option_file):
-        try:
-            f = open(option_file)
-            options = yaml.load(f)
-        except Exception as err:
-            print err.message
-        else:
-            if isinstance(options, dict):
-                sys = TradeSystem.find(id=options['id'])
-                if sys:
-                    if options.has_key('ip'):
-                        sys.ip = options['ip']
-                    if options.has_key('username'):
-                        sys.user = options['username']
-                    if options.has_key('password'):
-                        sys.password = options['password']
-                    db.session.add(sys)
-                    db.session.commit()
-            elif isinstance(options, list):
-                for config in options:
-                    sys = TradeSystem.find(id=config['id'])
-                    if sys:
-                        if config.has_key('ip'):
-                            sys.ip = config['ip']
-                        if config.has_key('username'):
-                            sys.user = config['username']
-                        if config.has_key('password'):
-                            sys.password = config['password']
-                        db.session.add(sys)
-                db.session.commit()
-    else:
-        print "file({}) not exists.".format(option_file)
-
-
-@manager.command
-def printurl():
-    print url_for('main.index'), url_for('main.adddevice')
-    print url_for('auth.login'), url_for('auth.register')
-    print url_for('api.users'), url_for('api.user', login='admin'), url_for('api.user', id=1)
-
-
-@manager.command
-def printuser():
-    for user in Operator.query.all():
-        print "username:", user.name
-        for role in user.roles:
-            print u"\trole name : ", role.name
-            print u"\t\tprivileges : "
-            for pri in role.privileges:
-                print u"\t\t\t", pri.uri, pri.bit
-
-
-@manager.command
-def printsys():
-    '''
-    for sys in TradeSystem.query.filter(TradeSystem.parent_sys_id==None).all():
-        print "system:", sys.name
-        for svr in sys.servers:
-            print "\tsvr info: {0} {1}".format(svr.name, svr.manage_ip)
-            for proc in [p for p in sys.processes if p.svr_id == svr.id]:
-                print "\t\tproc info: {0}({1})".format(proc.name, proc.id), proc.type
-        print "child systems:"
-        for child in sys.child_systems:
-            print "\t", child.name
-        print "up systems:"
-        for up in sys.up_systems:
-            print "\t", up.name
-        print "down systems:"
-        for down in sys.down_systems:
-            print "\t", down.name
-        print "administrators:"
-        for usr in sys.administrators:
-            print "\t", usr.name
-    proc = TradeProcess.find(id=12)
-    print proc.type.value
-    '''
-    svr = Server.query.filter()
-
-
-@manager.command
-def modeltest(page=1, all=False):
-    import time
-    page = int(page)
-    page_size = 10
-    query_begin = time.time()
-    if all:
-        records = OperateRecord.query.order_by(OperateRecord.operated_at.desc()).all()
-    else:
-        if page > 1:
-            records = OperateRecord.query.order_by(OperateRecord.operated_at.desc()) \
-                .offset((page - 1) * 10).limit(10).all()
-        else:
-            records = OperateRecord.query.order_by(OperateRecord.operated_at.desc()) \
-                .limit(10).all()
-        print 'page: {}'.format(page)
-    query_end = time.time()
-    print 'Query time: {} seconds.'.format(query_end - query_begin)
-    print_start = time.time()
-    for rec in records:
-        rec.to_json()
-    print_end = time.time()
-    print 'Print time: {} seconds.'.format(print_end - print_start)
+def decrypt():
+    pass
 
 
 @manager.command
@@ -389,9 +222,7 @@ def route_test():
     '''
     urls = test_app.url_map.bind('localhost')
     match = urls.match('api/user/id/1')
-    with test_app.request_context(
-            EnvironBuilder().get_environ()
-    ):
+    with test_app.request_context(EnvironBuilder().get_environ()):
         rsp = test_app.view_functions[match[0]](**match[1])
         print json.dumps(rsp.response)
 
