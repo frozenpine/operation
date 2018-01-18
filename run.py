3e24d6d838c723a602f2bc0f4ad7fe8b31d5fe10
袁暘 <yuanyang@quantdo.com.cn>
2018-01-18 11:11:54 +0800

    201801181111

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+""" Flask serrver entrypoint """
 import sys
 from os import environ
 
@@ -8,12 +9,12 @@ from geventwebsocket.handler import WebSocketHandler
 
 from app import create_app
 
-app_host = environ.get('FLASK_SVR') or '0.0.0.0'
-app_port = environ.get('FLASK_PORT') or 6001
+APP_HOST = environ.get('FLASK_SVR') or '0.0.0.0'
+APP_PORT = environ.get('FLASK_PORT') or 6001
 
 
 if __name__ == '__main__':
     monkey.patch_all()
-    app = create_app(sys.argv[1])
-    http_server = WSGIServer((app_host, int(app_port)), app, handler_class=WebSocketHandler)
-    http_server.serve_forever()
+    APP = create_app(sys.argv[1])
+    HTTP_SERVER = WSGIServer((APP_HOST, int(APP_PORT)), APP, handler_class=WebSocketHandler)
+    HTTP_SERVER.serve_forever()
